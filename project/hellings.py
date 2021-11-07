eae67bc4ee77b73eb1df5ae4754016ee4bb27e24
Vladislav Miroshnikov <vladislav.miroshnikov@jetbrains.com>
2021-11-08 00:22:55 +0300

    New changes in algorithm

--- project/hellings.py
+++ project/hellings.py
@@ -1,64 +1,75 @@
+from collections import Set
+from typing import Tuple
+
 import networkx as nx
-from pyformlang.cfg import CFG, Epsilon
+from pyformlang.cfg import CFG, Variable
 
 from project import convert_cfg_to_wcnf
 
-__all__ = ["hellings"]
+__all__ = ["hellings", "cfpq"]
 
 
 def hellings(cfg: CFG, graph: nx.MultiDiGraph):
     wcnf = convert_cfg_to_wcnf(cfg)
-    res = []
-    temp = []
-    for production in wcnf.productions:
-        if not production.body == [Epsilon()]:
-            continue
-        for v in graph.nodes:
-            res.append((v, production.head.value, v))
-            temp.append((v, production.head.value, v))
 
-    for u, c, v in graph.edges:
-        for production in wcnf.productions:
-            if len(production.body) == 0 or production.body[0].value == c:
-                res.append((u, production.head.value, v))
-                temp.append((u, production.head.value, v))
+    eps_prod_heads = [p.head.value for p in wcnf.productions if not p.body]
+    term_productions = {p for p in wcnf.productions if len(p.body) == 1}
+    var_productions = {p for p in wcnf.productions if len(p.body) == 2}
 
-    while temp:
-        temp_node_l, temp_variable, temp_node_r = temp.pop(0)
+    r = {(v, h, v) for v in range(graph.number_of_nodes()) for h in eps_prod_heads} | {
+        (u, p.head.value, v)
+        for u, v, edge_data in graph.edges(data=True)
+        for p in term_productions
+        if p.body[0].value == edge_data["label"]
+    }
 
-        for res_node_l, res_variable, res_node_r in res:
-            if res_node_r != temp_node_l:
-                continue
+    new = r.copy()
+    while new:
+        n, N, m = new.pop()
+        r_temp = set()
 
-            for production in wcnf.productions:
-                if len(production.body) <= 1:
-                    continue
-                if (
-                    production.body[0].value != res_variable
-                    or production.body[1].value != temp_variable
-                ):
-                    continue
-                if (res_node_l, production.head.value, temp_node_r) in res:
-                    continue
+        for u, M, v in r:
+            if v == n:
+                triplets = {
+                    (u, p.head.value, m)
+                    for p in var_productions
+                    if p.body[0].value == M
+                    and p.body[1].value == N
+                    and (u, p.head.value, m) not in r
+                }
+                new |= triplets
+                r_temp |= triplets
+        r |= r_temp
+        r_temp.clear()
 
-                temp.append((res_node_l, production.head.value, temp_node_r))
-                res.append((res_node_l, production.head.value, temp_node_r))
+        for u, M, v in r:
+            if u == m:
+                triplets = {
+                    (n, p.head.value, v)
+                    for p in var_productions
+                    if p.body[0].value == N
+                    and p.body[1].value == M
+                    and (n, p.head.value, v) not in r
+                }
+                new |= triplets
+                r_temp |= triplets
+        r |= r_temp
 
-        for res_node_l, res_variable, res_node_r in res:
-            if res_node_l != temp_node_r:
-                continue
+    return r
 
-            for production in wcnf.productions:
-                if len(production.body) <= 1:
-                    continue
-                if (
-                    production.body[0].value != temp_node_l
-                    or production.body[1].value != res_variable
-                ):
-                    continue
-                if (temp_node_l, production.head.value, res_node_r) in res:
-                    continue
 
-                temp.append((temp_node_l, production.head.value, res_node_r))
-                res.append((temp_node_l, production.head.value, res_node_r))
-    return res
+def cfpq(
+    graph: nx.MultiDiGraph,
+    cfg: CFG,
+    start_nodes: Set[int] = None,
+    final_nodes: Set[int] = None,
+    start_var: Variable = Variable("S"),
+) -> Set[Tuple[int, int]]:
+    cfg._start_symbol = start_var
+    reach_pairs = {(u, v) for u, h, v in hellings(cfg, graph) if h == cfg.start_symbol}
+    if start_nodes:
+        reach_pairs = {(u, v) for u, v in reach_pairs if u in start_nodes}
+    if final_nodes:
+        reach_pairs = {(u, v) for u, v in reach_pairs if v in final_nodes}
+
+    return reach_pairs
