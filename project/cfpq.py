e8299f70f166aff0dd98b618ed0918bbc9b75bb0
vladislav-miroshnikov <vladislaw.miroshnikov@gmail.com>
2021-12-13 21:07:21 +0300

    Return naming

--- project/cfpq.py
+++ project/cfpq.py
@@ -1,18 +1,18 @@
 from typing import Set, Tuple
+
 import networkx as nx
 from pyformlang.cfg import CFG, Variable
 
-from project import convert_cfg_to_wcnf
-from project.cfpq_algorithms import matrix, tensor
+from project.cfpq_algorithms import hellings, matrix, tensor
 
 __all__ = ["hellings_cfpq", "matrix_cfpq", "tensor_cfpq"]
 
 
 def _cfpq(
-    algorithm_result: Set[Tuple[int, str, int]],
-    cfg: CFG,
-    start_nodes: Set[int] = None,
-    final_nodes: Set[int] = None,
+        algorithm_result: Set[Tuple[int, str, int]],
+        cfg: CFG,
+        start_nodes: Set[int] = None,
+        final_nodes: Set[int] = None,
 ) -> Set[Tuple[int, int]]:
     """
     Internal function for CFPQ
@@ -43,11 +43,11 @@ def _cfpq(
 
 
 def hellings_cfpq(
-    graph: nx.MultiDiGraph,
-    cfg: CFG,
-    start_nodes: Set[int] = None,
-    final_nodes: Set[int] = None,
-    start_var: Variable = Variable("S"),
+        graph: nx.MultiDiGraph,
+        cfg: CFG,
+        start_nodes: Set[int] = None,
+        final_nodes: Set[int] = None,
+        start_var: Variable = Variable("S"),
 ) -> Set[Tuple[int, int]]:
     """
     Context-Free Path Querying based on Hellings Algorithm
@@ -72,78 +72,15 @@ def hellings_cfpq(
     """
     cfg._start_symbol = start_var
 
-    return _cfpq(set(_hellings_alg(graph, cfg)), cfg, start_nodes, final_nodes)
-
-def _hellings_alg(graph: nx.MultiDiGraph, cfg: CFG) -> set[Tuple[int, str, int]]:
-    """
-    Hellings algorithm for solving Context-Free Path Querying problem
-
-    Parameters
-    ----------
-    graph: nx.MultiDiGraph
-        input graph
-    cfg: CFG
-        input cfg
-
-    Returns
-    -------
-    set[Tuple[int, str, int]]:
-        set tuples (node, terminal, node)
-    """
-    wcnf = convert_cfg_to_wcnf(cfg)
-
-    eps_prod_heads = [p.head.value for p in wcnf.productions if not p.body]
-    term_productions = {p for p in wcnf.productions if len(p.body) == 1}
-    var_productions = {p for p in wcnf.productions if len(p.body) == 2}
-
-    r = {(v, h, v) for v in range(graph.number_of_nodes()) for h in eps_prod_heads} | {
-        (u, p.head.value, v)
-        for u, v, edge_data in graph.edges(data=True)
-        for p in term_productions
-        if p.body[0].value == edge_data["label"]
-    }
-
-    new = r.copy()
-    while new:
-        n, N, m = new.pop()
-        r_temp = set()
-
-        for u, M, v in r:
-            if v == n:
-                triplets = {
-                    (u, p.head.value, m)
-                    for p in var_productions
-                    if p.body[0].value == M
-                    and p.body[1].value == N
-                    and (u, p.head.value, m) not in r
-                }
-                new |= triplets
-                r_temp |= triplets
-        r |= r_temp
-        r_temp.clear()
-
-        for u, M, v in r:
-            if u == m:
-                triplets = {
-                    (n, p.head.value, v)
-                    for p in var_productions
-                    if p.body[0].value == N
-                    and p.body[1].value == M
-                    and (n, p.head.value, v) not in r
-                }
-                new |= triplets
-                r_temp |= triplets
-        r |= r_temp
-
-    return r
+    return _cfpq(set(hellings(graph, cfg)), cfg, start_nodes, final_nodes)
 
 
 def matrix_cfpq(
-    graph: nx.MultiDiGraph,
-    cfg: CFG,
-    start_nodes: Set[int] = None,
-    final_nodes: Set[int] = None,
-    start_variable: Variable = Variable("S"),
+        graph: nx.MultiDiGraph,
+        cfg: CFG,
+        start_nodes: Set[int] = None,
+        final_nodes: Set[int] = None,
+        start_variable: Variable = Variable("S"),
 ) -> Set[Tuple[int, int]]:
     """
     Context-Free Path Querying based on matrix multiplication
@@ -172,11 +109,11 @@ def matrix_cfpq(
 
 
 def tensor_cfpq(
-    graph: nx.MultiDiGraph,
-    cfg: CFG,
-    start_nodes: Set[int] = None,
-    final_nodes: Set[int] = None,
-    start_variable: Variable = Variable("S"),
+        graph: nx.MultiDiGraph,
+        cfg: CFG,
+        start_nodes: Set[int] = None,
+        final_nodes: Set[int] = None,
+        start_variable: Variable = Variable("S"),
 ) -> Set[Tuple[int, int]]:
     """
     Context-Free Path Querying based on tensor algorithm and RSM
--- project/cfpq_algorithms.py
+++ project/cfpq_algorithms.py
@@ -12,7 +12,72 @@ from project import (
     intersect_boolean_matrices,
 )
 
-__all__ = ["matrix", "tensor"]
+__all__ = ["hellings", "matrix", "tensor"]
+
+
+def hellings(graph: nx.MultiDiGraph, cfg: CFG) -> set[Tuple[int, str, int]]:
+    """
+    Hellings algorithm for solving Context-Free Path Querying problem
+
+    Parameters
+    ----------
+    graph: nx.MultiDiGraph
+        input graph
+    cfg: CFG
+        input cfg
+
+    Returns
+    -------
+    set[Tuple[int, str, int]]:
+        set tuples (node, terminal, node)
+    """
+    wcnf = convert_cfg_to_wcnf(cfg)
+
+    eps_prod_heads = [p.head.value for p in wcnf.productions if not p.body]
+    term_productions = {p for p in wcnf.productions if len(p.body) == 1}
+    var_productions = {p for p in wcnf.productions if len(p.body) == 2}
+
+    r = {(v, h, v) for v in range(graph.number_of_nodes()) for h in eps_prod_heads} | {
+        (u, p.head.value, v)
+        for u, v, edge_data in graph.edges(data=True)
+        for p in term_productions
+        if p.body[0].value == edge_data["label"]
+    }
+
+    new = r.copy()
+    while new:
+        n, N, m = new.pop()
+        r_temp = set()
+
+        for u, M, v in r:
+            if v == n:
+                triplets = {
+                    (u, p.head.value, m)
+                    for p in var_productions
+                    if p.body[0].value == M
+                       and p.body[1].value == N
+                       and (u, p.head.value, m) not in r
+                }
+                new |= triplets
+                r_temp |= triplets
+        r |= r_temp
+        r_temp.clear()
+
+        for u, M, v in r:
+            if u == m:
+                triplets = {
+                    (n, p.head.value, v)
+                    for p in var_productions
+                    if p.body[0].value == N
+                       and p.body[1].value == M
+                       and (n, p.head.value, v) not in r
+                }
+                new |= triplets
+                r_temp |= triplets
+        r |= r_temp
+
+    return r
+
 
 def matrix(graph: nx.MultiDiGraph, cfg: CFG) -> set[Tuple[int, str, int]]:
     """
@@ -56,7 +121,7 @@ def matrix(graph: nx.MultiDiGraph, cfg: CFG) -> set[Tuple[int, str, int]]:
         for p in variable_productions:
             old_nnz = matrices[p.head.value].nnz
             matrices[p.head.value] += (
-                matrices[p.body[0].value] @ matrices[p.body[1].value]
+                    matrices[p.body[0].value] @ matrices[p.body[1].value]
             )
             new_nnz = matrices[p.head.value].nnz
             changed = changed or old_nnz != new_nnz
