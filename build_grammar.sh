1e7b1d0652eb7f04e5379b31e65d928f9d5b6fbe
vladislav-miroshnikov <55508043+vladislav-miroshnikov@users.noreply.github.com>
2022-01-01 12:55:58 +0300

    Task14 parser (#15)

---
+++ build_grammar.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+antlr4 -Dlanguage=Python3 ./project/graph_query_language/GraphQueryLanguage.g4
---
+++ project/parser.py
@@ -0,0 +1,25 @@
+from antlr4 import InputStream, CommonTokenStream
+
+from project.graph_query_language.GraphQueryLanguageLexer import GraphQueryLanguageLexer
+from project.graph_query_language.GraphQueryLanguageParser import (
+    GraphQueryLanguageParser,
+)
+
+__all__ = ["parse", "check_parser_correct"]
+
+
+def parse(text: str) -> GraphQueryLanguageParser:
+    input_stream = InputStream(text)
+    lexer = GraphQueryLanguageLexer(input_stream)
+    lexer.removeErrorListeners()
+    stream = CommonTokenStream(lexer)
+    parser = GraphQueryLanguageParser(stream)
+
+    return parser
+
+
+def check_parser_correct(text: str) -> bool:
+    parser = parse(text)
+    parser.removeErrorListeners()
+    _ = parser.prog()
+    return parser.getNumberOfSyntaxErrors() == 0
